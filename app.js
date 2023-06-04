f28b39fb5df0d499886df6d67406219e6cda60f8
Haider <125460927+haider36@users.noreply.github.com>
2023-06-04 16:21:46 +0500

    add the title and logo

---
+++ app.js
@@ -0,0 +1,123 @@
+var questions = [
+  {
+    question: 'What does HTML stand for?',
+    options1: 'Hyper Text Markup Language',
+    options2: 'Home Tool Markup Language',
+    options3: 'Hyperlinks and Text Markup Language',
+    options4: 'Hyper Text Markup Leveler',
+    correctAnswer: 'Hyper Text Markup Language'
+  },
+  {
+    question: 'which HTML element is used to define a paragraph?',
+    options1: '<p>',
+    options2: '<h1>',
+    options3: '<a>',
+    options4: '<div>',
+    correctAnswer: '<p>'
+  },
+  {
+    question: 'What is the correct HTML for adding an image?',
+    options1: '<img src="image.jpg" alt="MyImage">',
+    options2: '<img alt="MyImage">image.jpg</img>',
+    options3: '<image src="image.jpg" alt="MyImage">',
+    options4: '<img href="image.jpg" alt="MyImage">',
+    correctAnswer: '<img src="image.jpg" alt="MyImage">'
+  },
+  {
+    question: 'Which tag is used to define the header of a section or article in HTML5?',
+    options1: '</header>',
+    options2: '<h1>',
+    options3: '<heading>',
+    options4: '<hgroup>',
+    correctAnswer: " <header></header>"
+  },
+  {
+    question: 'Which tag is used to define the header of a section or article in HTML5?',
+    options1: '</header>',
+    options2: '<h1>',
+    options3: '<heading>',
+    options4: '<hgroup>',
+    correctAnswer: " <header></header>"
+  },
+  {
+    question: 'How do you create a line break in HTML?',
+    options1: '<br>',
+    options2: '<ib>',
+    options3: '<break>',
+    options4: '<line>',
+    correctAnswer: " <br>"
+  },
+  {
+  question: 'Which tag is used to define a table row in HTML?',
+    options1: '<td>',
+    options2: '<tr>',
+    options3: '<table>',
+    options4: '<th>',
+    correctAnswer: " <tr>"
+  },
+  {
+    question: 'How do you create an unordered list in HTML?',
+      options1: '<ul>',
+      options2: '<ol>',
+      options3: '<li>',
+      options4: '<list>',
+      correctAnswer: " <ul>"
+    },
+    {
+      question: 'How do you create a hyperlink in HTML?',
+        options1: '<a href="url">link text</a>',
+        options2: ' <link href="url">link text</link>',
+        options3: '<a src="url">link text</a>',
+        options4: '<a>link text</a>',
+        correctAnswer: '<a href="url">link text</a>'
+      },
+      {
+        question: 'How do you add comments in HTML?',
+          options1: '<!-- This is a comment -->',
+          options2: '/ This is a comment',
+          options3: '<!--- This is a comment --->',
+          options4: ' // This is a comment //',
+          correctAnswer: '<!-- This is a comment -->'
+        },
+        {
+          question: 'The <title> element must be located inside?',
+            options1: 'the <head> element',
+            options2: 'the <body> element',
+            options3: 'the end <body> element',
+            options4: 'the <title> element',
+            correctAnswer: 'the <head> element'
+          },
+          {
+            question: 'Where we can use the <style> tag?',
+              options1: 'In the <head> element',
+              options2: 'In the <body> element',
+              options3: 'the end <body> element',
+              options4: 'In the <head> and <body> elements',
+              correctAnswer: 'In the <head> and <body> elements'
+            },
+            {
+              question: 'How you can open the link in a new window?',
+                options1: 'target="_new"',
+                options2: 'target="_window"',
+                options3: 'target="_blank"',
+                options4: 'target="_value"',
+                correctAnswer: 'target="_blank"'
+              },
+              {
+                question:'<h6> is the smallest heading tag?',
+                options1:'True',
+                options2:'Fals',
+                correctAnswer:'true'
+              },
+              {
+                question:'The <br> tag is used to add?',
+                options1:'space',
+                options2:'line break',
+                options3:'word break',
+                options4:'paragraph break',
+                correctAnswer:'line break'
+              }
+  ];
+
+  var ques = document.getElementById("question").innerHTML.valueOf
+ 
